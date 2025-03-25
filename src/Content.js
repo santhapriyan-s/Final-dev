2589cc09c2517d110e3a73db6ea180940d6d00db
SANTHAPRIYAN S <santhapriyansubramani9@gmail.com>
2025-03-25 13:39:36 +0530

    Update Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Content.css'; // Import Content-specific CSS
+
 const Content = () => {
-return (
-<section className="content">
-<h1>Welcome to My Website</h1>
-<p>This is a description about the project. Here we can talk about the mission, vision,
-and goals of the project.</p>
-<img src="./d.jpg" alt="Project Image" />
-</section>
-);
+  return (
+    <section className="content">
+      <h1>Welcome to My Website</h1>
+      <p>
+        This is a description about the project. Here we can talk about the mission, vision,
+        and goals of the project.
+      </p>
+      <img src="./d.jpg" alt="Project" /> {/* Fixed alt text */}
+    </section>
+  );
 };
+
 export default Content;
